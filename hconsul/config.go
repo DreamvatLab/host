797017e8f3911d0a92a39ebef070260ac54dafc9
Lukiya Chen <lukiya@outlook.com>
2025-05-01 15:19:33 -0700

    Update dependencies to version 1.0.9 for go and oauth2go in go.sum and remove unused GetConsulConfig function from config.go.

--- hconsul/config.go
+++ hconsul/config.go
@@ -1,17 +1,6 @@
 package hconsul
 
-import "github.com/DreamvatLab/go/xconfig"
-
 type ConsulConfig struct {
 	Addr  string
 	Token string
-}
-
-func GetConsulConfig(configProvider xconfig.IConfigProvider) (*ConsulConfig, error) {
-	var consulConfig *ConsulConfig
-	err := configProvider.GetStruct("Consul", &consulConfig)
-	if err != nil {
-		return nil, err
-	}
-	return consulConfig, nil
 }
